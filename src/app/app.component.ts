5b263ac07c8093141c35ac67d70f36ae1b72c479
Jameel Moufak <jameelmoufak33@gmail.com>
2024-09-11 01:29:23 +0300

    create with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
