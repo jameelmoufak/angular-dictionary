3f6b700d4d74ca73fcf923636d46dc3465245097
Nouri <nouryman999@gmail.com>
2024-09-14 04:27:01 +0300

    Line comments - using get,set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,60 @@
 import { Component, OnDestroy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { dictionaryservice } from './dictionary.service';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { Idictionary } from './dictionary';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabsModule, FormsModule, CommonModule],
+  imports: [TabsModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 
 export class AppComponent implements OnDestroy {
   constructor(private dictionaryservice: dictionaryservice) { }
 
-  searchWord: string = "";
-  dicInfo: Idictionary | undefined;
-  audiosrc: any = '';
-  wordsearchnotfound: string = '';
-  subscription?: Subscription;
-  found: boolean = false;
-  getDictionary() {
-    this.subscription = this.dictionaryservice.getDicionary(this.searchWord).
+  dicInfo: Idictionary | undefined;//to get dictionary info
+  audiosrc: string = '';//to get src of audio 
+  subscription?: Subscription;//for OnDestroy 
+  found: boolean = false;//to check if search word exist 
+  private _searchWord: string = ''//to bind input text
+  public get SearchWord() {
+    return this._searchWord;
+  }
+  public set SearchWord(value: string) {
+    this._searchWord = value;
+
+    this.subscription = this.dictionaryservice.getDicionary(this._searchWord).
       subscribe(
         (response => {
+          //here set properties values if search word is exist 
           this.dicInfo = response;
           this.found = true;
+          this.audiosrc = ''
+          for (let i = 0; i < this.dicInfo?.phonetics.length; i++) {
+            if (this.dicInfo?.phonetics[i].audio != '') {
+              this.audiosrc = this.dicInfo?.phonetics[i].audio;
+              break;
+            }
+          }
         }),
-        (() => this.found = false)
+        //if search word is not exist 
+        (() => this.found = false),
       );
-
   }
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
   }
+  //to play audio
   playaudio() {
-    for (let i = 0; i < this.dicInfo?.phonetics.length; i++) {
-      if (this.dicInfo?.phonetics[i].audio != '') {
-        this.audiosrc = this.dicInfo?.phonetics[i].audio;
-        break;
-      }
-    }
     let audio = new Audio();
     audio.src = this.audiosrc;
-    this.wordsearchnotfound = '';
+    console.log(audio.src)
     audio.play()
       .catch(() => {
         console.log('the word search is not found');
-        this.wordsearchnotfound = "the word search is not found"
       });
   }
 }
--- src/app/dictionary.service.ts
+++ src/app/dictionary.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Idictionary } from "./dictionary";
-import { catchError, EMPTY, map, Observable, throwError } from "rxjs";
+import { catchError, map, Observable, throwError } from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
 export class dictionaryservice {
   constructor(private http: HttpClient) { }
+  // to get data from api 
   getDicionary(word: string): Observable<Idictionary> {
     return this.http.get<Idictionary[]>(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).pipe(
       map(data => {
+        //to define dictionary properties
         let dic: Idictionary = {
           word: data[0].word,
           phonetic: data[0].phonetic,
